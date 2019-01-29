bcc6cbbaa1df07534e28084257c3d40bc420d774
Dara Javaherian <darajavaherian@gmail.com>
2019-01-30 00:13:29 +0100

    add build

---
+++ build/precache-manifest.1683b27775610f81cdef20ee521adb3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ccb8dcf6efd5fe6c4e1",
+    "url": "/turtle-challenge/static/css/main.2e22fb23.chunk.css"
+  },
+  {
+    "revision": "2ccb8dcf6efd5fe6c4e1",
+    "url": "/turtle-challenge/static/js/main.2ccb8dcf.chunk.js"
+  },
+  {
+    "revision": "c2be315a75845363e6a7",
+    "url": "/turtle-challenge/static/js/1.c2be315a.chunk.js"
+  },
+  {
+    "revision": "851ff629ddb7358f615c",
+    "url": "/turtle-challenge/static/js/runtime~main.851ff629.js"
+  },
+  {
+    "revision": "108bded60bd425cd88c4512b7558bf4f",
+    "url": "/turtle-challenge/index.html"
+  }
+];
