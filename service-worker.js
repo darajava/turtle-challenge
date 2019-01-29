5b81b6ba6e7c2d4c6dc18dcd78e7a6b3a4b5f273
Dara Javaherian <darajavaherian@gmail.com>
2019-01-30 00:24:57 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/turtle-challenge/build/precache-manifest.aa19afc5c826b64fb8dddea9444f3356.js"
+  "/turtle-challenge/precache-manifest.1683b27775610f81cdef20ee521adb3e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/turtle-challenge/build/index.html", {
+workbox.routing.registerNavigationRoute("/turtle-challenge/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
