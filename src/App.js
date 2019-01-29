e2326dd996e08fac0c71d3e4f06baa2994f675b2
Dara Javaherian <darajavaherian@gmail.com>
2019-01-29 22:45:23 +0100

    add json files, and get inital grid working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+import moves from './data/moves_0.json';
+import map from './data/map_0.json';
+
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      player: map.startPos,
+    }
+  }
+
+
+  isMine(x, y) {
+    for (let i = 0; i < map.mines.length; i++) {
+      if (map.mines[i].x === x && map.mines[i].y === y) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+
+  isExit(x, y) {
+    return map.exitPos.x === x && map.exitPos.y === y;
+  }
+
+
+  isPlayerPos(x, y) {
+    return this.state.player.x === x && this.state.player.y === y
+  }
+
+
+  generateGrid() {
+    const gridChildren = [];
+
+    for (let y = 0; y < map.size.y; y++) {
+      for (let x = 0; x < map.size.x; x++) {
+        let child = " ";
+        if (this.isMine(x, y)) {
+          child = "mine";
+        } else if (this.isExit(x, y)) {
+          child = "exit";
+        } else if (this.isPlayerPos(x, y)) {
+          child = "turtle";
+        }
+
+
+        gridChildren.push(<span className="grid-item" data-x={x} data-y={y}>{child}</span>);
+      }
+      gridChildren.push(<div/>)
+    }
+
+    return gridChildren;
+  }
+
   render() {
+
+    console.log(moves);
+    console.log(map);
+
+
+    const gridChildren = this.generateGrid();
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="table-holder">
+        {gridChildren}
       </div>
     );
   }
